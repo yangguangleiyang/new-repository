714f050d8b9e1283613d8addb0f92ce3e21153e4
yanglei <15712895616@163.com>
2020-10-19 06:10:29 +0800

    无任何新增

--- fullstack/week3/day10/hashlib_module.py
+++ fullstack/week3/day10/hashlib_module.py
@@ -2,12 +2,12 @@
 
 import hashlib
 
-# m=hashlib.md5()     #通过md5的算法加密
-# m.update("hello world".encode("utf8"))
-# print(m.hexdigest())    #作为十六进制数据字符串值   5eb63bbbe01eeed093cb22bb8f5acdc3
+m=hashlib.md5()     #通过md5的算法加密
+m.update("hello world".encode("utf8"))
+print(m.hexdigest())    #作为十六进制数据字符串值   5eb63bbbe01eeed093cb22bb8f5acdc3
 # print(m.digest())    # 作为二进制数据字符串值  b'^\xb6;\xbb\xe0\x1e\xee\xd0\x93\xcb"\xbb\x8fZ\xcd\xc3'
 
 #另一种算法
-s=hashlib.sha256()
-s.update("hello world".encode("utf8"))
-print(s.hexdigest())
+# s=hashlib.sha256()
+# s.update("hello world".encode("utf8"))
+# print(s.hexdigest())
