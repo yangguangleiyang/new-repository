ae241cd436abb2b72d85f41a8bf01dca3400f804
yangguangleiyang <63392918+yangguangleiyang@users.noreply.github.com>
2020-10-30 14:33:08 +0800

    Update today.py

--- fullstack/week3/day10/today.py
+++ fullstack/week3/day10/today.py
@@ -1,3 +1,4 @@
-print("hello,world 123456789")
+print("hello,world 123456789abcello,world 123456789abcello,world 123456789abc")
 print("停车坐爱枫林晚")
+print("hello,world 123456789abcello,world 123456789abcello,world 123456789abc")
 print("霜叶红于二月花123456789")
