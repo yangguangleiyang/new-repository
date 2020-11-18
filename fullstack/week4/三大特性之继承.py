01b91b95964487776bff0ca3c2cb604281f56ceb
yanglei <15712895616@163.com>
2020-11-18 21:47:59 +0800

    11-18日在家学习多继承

--- fullstack/week4/三大特性之继承.py
+++ fullstack/week4/三大特性之继承.py
@@ -1,23 +1,29 @@
-class F:
+# class F:
+#
+#     def f1(self):
+#         print("F.f1")
+#
+#     def f2(self):
+#         print("F.f2")
+#
+# class S(F):
+#
+#     def s1(self):
+#         print("S.s1")
+#
+#     def s2(self):
+#         print("F.f2")
+#
+#     def f2(self):       #如果不想用父类的方法可以重写
+#         print("重写f2")
+#         super(S,self).f2()  #如果子类和父类都有同一方法，这样就可以同时调用了
+#         F.f2(self)         #同上面一样，写法不同
+# obj=S()
+# obj.s1()
+# obj.f2()
 
-    def f1(self):
-        print("F.f1")
 
-    def f2(self):
-        print("F.f2")
-
-class S(F):
-
-    def s1(self):
-        print("S.s1")
-
-    def s2(self):
-        print("F.f2")
-
-    def f2(self):       #如果不想用父类的方法可以重写
-        print("重写f2")
-        super(S,self).f2()  #如果子类和父类都有同一方法，这样就可以同时调用了
-        F.f2(self)         #同上面一样，写法不同
-obj=S()
-obj.s1()
-obj.f2()
+#python中支持多继承
+#1.左侧优先
+#2.一条道走到黑
+#3.同一个根时，根最后执行
