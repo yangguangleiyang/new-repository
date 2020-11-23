e8f8bf6c9aed762b7c44a0ba863e44c289636faf
yangguangleiyang <15712895616@163.com>
2020-11-23 19:56:57 +0800

    11-23company

--- fullstack/week4/成员修饰符.py
+++ fullstack/week4/成员修饰符.py
@@ -1,6 +1,6 @@
 #公有字段、方法
 #私有字段、方法
-
+#
 class F:
     def __init__(self):
         self.name=123
@@ -23,13 +23,13 @@ obj.show()
 #1.私有字段或方法不能被外部调用（外部指的是类），类内可以调用，但是可以间接调用，被同类里面别的方法调用后，再创建对象，
 #调用别的方法
 
-# class A:
-#     def test(self):
-#         print("yang")
-#
-# class B(A):
-#     def test(self):
-#         print("xiaoyang")
-#         super(B,self).test()   #子类和父类同时调用
-# obj=B()
-# obj.test()
+class A:
+    def test(self):
+        print("yang")
+
+class B(A):
+    def test(self):
+        print("xiaoyang")
+        super(B,self).test()   #子类和父类同时调用
+obj=B()
+obj.test()
