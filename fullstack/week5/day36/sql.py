0f045d10b55d73a6c93a07e7c59186b4e92381e6
yangguangleiyang <15712895616@163.com>
2021-01-23 16:19:19 +0800

    重新学习pymysql

--- fullstack/week5/day36/sql.py
+++ fullstack/week5/day36/sql.py
@@ -1,16 +1,53 @@
+# pymysql
+#    --专门用于操作mysql的python模块
+#    --Mysqldb(py3不支持)
+'''
 import pymysql
 
 #创建连接
-conn=pymysql.connect(host="127.0.0.1",port=3306,user="root",passwd="123456",db="test1",charset="utf8")
+conn = pymysql.connect(host="127.0.0.1",port=3306,user="root",passwd="123456",db="test1")
 #创建游标
 cursor = conn.cursor()
 
-#cursor.execute("insert into class(caption)values('一年级二班')")    #记住此处是单引号！！！
+inp=input("请输入班级:")
+cursor.execute('insert into class(caption)values(%s)',inp)
+#cursor.execute('insert into class(caption,name,age)values(%s,%s,%s)',("自学全栈三班","鸭蛋","18"))
 
-#参数传递，必须使用参数形式
-#inp=input("请输入班级")
-cursor.execute("insert into student(gender,class_id,sname)values(%s,%s,%s)",('男',2,'小丁'))
+#r=cursor.execute('insert into class(caption)values("自学全栈一班")')  其中r为返回值，意思是受影响的行数
+#print(r)
+
+
 conn.commit()
+
+#关闭游标
+cursor.close()
+#关闭连接
+conn.close()
+
+'''
+
+#以上为增、删、改操作，都一样
+#--------------------------------------
+#以下为查询操作,(没有commit)
+
+import pymysql
+
+#创建连接
+conn = pymysql.connect(host="127.0.0.1",port=3306,user="root",passwd="123456",db="test1")
+#创建游标
+cursor = conn.cursor()
+
+cursor.execute("select * from class")
+
+# result=cursor.fetchall()   #取出所有的
+# print(result)
+
+result=cursor.fetchone()   #取出第一个
+print(result)
+
+# result=cursor.fetchmany(3)   #取出前三个
+# print(result)
+
 
 #关闭游标
 cursor.close()
@@ -19,10 +56,12 @@ conn.close()
 
 
 
-#备注
-#增、删、改 需要有conn.commit()    查没有
-#查询
-cursor.execute("select * from student")
-result=cursor.fetchall()   #打印出所有的
-print(result)
-result=cursor.fetchone()   #打印出第一个
+
+
+
+
+
+
+
+
+
--- fullstack/week5/day37/src/commons.py
+++ fullstack/week5/day37/src/commons.py
@@ -1,3 +1,1 @@
 
-def add():
-    print("add")
