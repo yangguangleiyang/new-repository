78a460f6de139f8b5417396abd36be8d74a7d817
yangguangleiyang <15712895616@163.com>
2021-03-04 19:09:48 +0800

    unittest firth day

---
+++ fullstack/unittest/ecshop_login.py
@@ -0,0 +1,11 @@
+import unittest
+
+class EcshopLogin(unittest.TestCase):
+    def test01_baili(self):
+        print("测试百里")
+
+    def test01_weiwei(self):
+        print("测试微微")
+
+if __name__ == '__main__':
+    unittest.main()
